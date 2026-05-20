31c366760c92aa9e89f9ddcf40fb39cbe818f813
HoopsOS VSA Refactor <refactor@hoopsos.dev>
2026-05-20 18:50:28 +0000

    debug: wrap createApp in try/catch to expose init errors on Vercel

--- server/_vercel_entry.ts
+++ server/_vercel_entry.ts
@@ -1,2 +1,21 @@
 import { createApp } from "../server/app";
-export default createApp();
+
+let _handler: ReturnType<typeof createApp> | null = null;
+let _err: unknown = null;
+
+try {
+  _handler = createApp();
+} catch (e) {
+  _err = e;
+}
+
+export default function handler(req: any, res: any) {
+  if (_err || !_handler) {
+    const msg = _err instanceof Error ? _err.message : "init failed";
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ init_error: msg }));
+    return;
+  }
+  (_handler as any)(req, res);
+}
